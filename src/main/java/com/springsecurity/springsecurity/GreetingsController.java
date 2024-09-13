1d8baa390546e59ee3e81e2ff35a3ec4a5eb7ef4
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2024-09-14 02:13:42 +0530

    Role Based Authorization

--- src/main/java/com/springsecurity/springsecurity/GreetingsController.java
+++ src/main/java/com/springsecurity/springsecurity/GreetingsController.java
@@ -1,6 +1,7 @@
 package com.springsecurity.springsecurity;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello Admin";
     }
 }
